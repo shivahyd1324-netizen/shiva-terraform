e030a309602b7c073e6acf6030160b656f9611d7
shivahyd1324-netizen <shivahyd1324@gmail.com>
2025-10-15 13:19:34 +0530

    update

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform4" {
-      count = 4
+      count = 10
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all4.id]
