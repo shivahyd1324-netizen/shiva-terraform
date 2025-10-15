372423bf29ca39fca9031431d1e506f84cf65614
shivahyd1324-netizen <shivahyd1324@gmail.com>
2025-10-15 18:32:13 +0530

    updated

--- count/route53.tf
+++ count/route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "shivaroboshop" {
-    count = 4
+    count = length(var.instances)
   zone_id = "${var.zone_id}"
   name    = "${var.instances[count.index]}.${var.domaine_name}" # mongodb.shivadevops86s.fun
   type    = "A"
